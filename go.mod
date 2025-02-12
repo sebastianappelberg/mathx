b76a2e14a48b91d4e2651d7ef1f853c2acf35a91
sebastian <sebastian.appelberg@gmail.com>
2025-02-12 15:41:22 +0100

    refactor: removed golang.org/x/exp/constraints dependency by inlining the types

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/sebastianappelberg/mathx
 
 go 1.23
-
-require golang.org/x/exp v0.0.0-20250210185358-939b2ce775ac // indirect
--- types.go
+++ types.go
@@ -1,7 +1,21 @@
 package mathx
 
-import "golang.org/x/exp/constraints"
-
 type Number interface {
-	constraints.Integer | constraints.Float
+	Integer | Float
+}
+
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type Integer interface {
+	Signed | Unsigned
+}
+
+type Float interface {
+	~float32 | ~float64
 }
